c580d89b0869177f05d59bc33ac0f69ed9978538
Avery Yip <averyyip@ymail.com>
2017-10-19 19:26:01 -0700

    Completed Linking Pages and Small Menu

--- app/views.py
+++ app/views.py
@@ -1,7 +1,48 @@
+from flask import render_template
 from app import app
+
+"""
+Tips for making templates and linking with handlers
+
+{{ex}} is for dynamic content
+use "ex" as an argument in the flask handler 
+
+
+ 
+Example of control statements (utilizes Jinja2 templates)
+{% if title %}
+	<title>{{ title }} - microblog</title>
+{% else %}
+    <title>Welcome to microblog</title>
+{% endif %} 
+
+
+
+Example of for loops
+
+Handler return statement
+posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+
+<body>
+    <h1>Hi, {{ user.nickname }}!</h1>
+    {% for post in posts %}
+    <div><p>{{ post.author.nickname }} says: <b>{{ post.body }}</b></p></div>
+    {% endfor %}
+ </body>
+"""
 
 #Mappings from Routes / and /index to Python Flask handler below
 @app.route("/")
 @app.route("/index")
 def index():
-	return "Hello, World!"
+	return render_template("index.html")
+
